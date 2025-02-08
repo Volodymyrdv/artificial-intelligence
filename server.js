11950fb7104cd233e4c2a641c7fb54dccdd53699
Volodymyrdv <vovakryt123453@gmail.com>
2025-02-09 00:53:44 +0200

    Add tsks.json

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import fs from 'fs';
 
 const app = express();
 const port = 5000;
@@ -8,7 +9,21 @@ const port = 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-let tasks = [];
+const tasksFilePath = 'tasks.json';
+
+const loadTasksFromFile = () => {
+	if (fs.existsSync(tasksFilePath)) {
+		const rawData = fs.readFileSync(tasksFilePath);
+		return JSON.parse(rawData);
+	}
+	return [];
+};
+
+const saveTasksToFile = () => {
+	fs.writeFileSync(tasksFilePath, JSON.stringify(tasks, null, 2));
+};
+
+let tasks = loadTasksFromFile();
 
 app.get('/tasks', (req, res) => {
 	res.json({ tasks });
@@ -17,6 +32,7 @@ app.get('/tasks', (req, res) => {
 app.put('/tasks', (req, res) => {
 	const { tasks: updatedTasks } = req.body;
 	tasks = updatedTasks;
+	saveTasksToFile();
 	res.status(200).json({ message: 'Tasks updated' });
 });
 
@@ -28,12 +44,14 @@ app.post('/tasks', (req, res) => {
 		completed: false
 	};
 	tasks.push(newTask);
+	saveTasksToFile();
 	res.status(201).json(newTask);
 });
 
 app.delete('/tasks/:id', (req, res) => {
 	const { id } = req.params;
 	tasks = tasks.filter((task) => task.id != id);
+	saveTasksToFile();
 	res.status(200).json({ message: 'Task deleted' });
 });
 
